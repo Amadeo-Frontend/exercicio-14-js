88478132b03fbb29cadb90df57f7de496fec7d71
Amadeo Bon <amadeosph@outlook.com>
2022-06-30 02:41:08 -0300

    exe

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var branco = "preto"
+var preto = "cinza"
+var cinza = "branco"
+var carro = "preto"
+var valor = 30000
+var prestacao = 750
+
+//f)Somando as variáveis de cores é formada uma string de quantos caracteres?
+
+caracteres = 'pretocinzabranco'
+resultado = caracteres.length
+//console.log(resultado)
